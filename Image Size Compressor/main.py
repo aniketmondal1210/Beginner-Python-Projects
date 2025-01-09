1e6f006d62a777d55eab805abbba840bd32e5939
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-01-09 23:55:51 +0530

    Update main.py

--- Image Size Compressor/main.py
+++ Image Size Compressor/main.py
@@ -15,7 +15,7 @@ def compress_image(image_path, output_path, quality=20):
     except Exception as e:
         print(f"An error occurred: {e}")
 
-input_image = input("Enter the path of the image you want to compress: ")  # Example: "C:/Users/ASUS/Pictures/image.jpg"
+input_image = input("Enter the path of the image you want to compress: ")  # Example: "C:/Users/ASUS/Pictures/test_image.jpg"
 output_image = input("Enter the path to save the compressed image: ")  # Example: "C:/Users/ASUS/Pictures/compressed_image.jpeg"
 
 # Call the function
