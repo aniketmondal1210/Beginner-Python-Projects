ddaf29d6a183959ec7b43be718cfbc36794c4417
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-01-25 00:14:07 +0530

    Update main.py

--- Speech to Text Converter/main.py
+++ Speech to Text Converter/main.py
@@ -2,14 +2,16 @@ import speech_recognition as sr
 import pyaudio
 
 def speech_to_text():
-  recognizer = sr.Recognizer()
+    recognizer = sr.Recognizer()
 
-with sr.Microphone() as source:
-  recognizer.adjust_for_ambient_noise(source)
+    with sr.Microphone() as source:
+        recognizer.adjust_for_ambient_noise(source)
 
-  print("Listening to you!...")
+        print("Say something: Listening to your voice...")
 
-  audio = recognizer.listen(source)
+        audio = recognizer.listen(source)
 
-  text = recognizer.recognize_google(audio)
-  print(text.upper())
+        text = recognizer.recognize_google(audio)
+        print("You said:", text.capitalize())
+
+speech_to_text()
